5076b75bcb582ba70e58e956bf0c1caadc3e043f
Bin Xia <quanbinn@126.com>
2016-03-31 00:04:19 -0700

    test routes for every component of food-extract

--- lib/routes.jsx
+++ lib/routes.jsx
@@ -9,9 +9,25 @@ Router.route('/', () => {
 	React.render(app, document.body);
 });
 
-Router.route('/test', function () {
+Router.route('/test1', function () {
+  React.render(<HighFatPercentFood />, document.body);
+});
+Router.route('/test2', function () {
   React.render(<HighSaturatedFatPercentFood />, document.body);
 });
+Router.route('/test3', function () {
+  React.render(<HighCholestorelPercentFood />, document.body);
+});
+Router.route('/test4', function () {
+  React.render(<SuperhighFatPercentFood />, document.body);
+});
+Router.route('/test5', function () {
+  React.render(<SuperhighSaturatedFatPercentFood />, document.body);
+});
+Router.route('/test6', function () {
+  React.render(<SuperhighCholestorelPercentFood />, document.body);
+});
+
 
 Router.route('/addfood', function () {
   React.render(<Addfood />, document.body);
